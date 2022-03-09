246d2124b6a23ba8147c8518408345075f8c4cc7
channpreet1991 <65122644+channpreet1991@users.noreply.github.com>
2022-03-08 20:28:55 -0400

    1st commit

---
+++ Loop Assignment.cs
@@ -0,0 +1,32 @@
+
+while statement
+
+ 
+
+class WhileDemo {
+   public static void main(String[] args){
+       int count = 1;
+       while (count < 11) {
+           System.out.println("Count is: " + count);
+           count++;
+       }
+   }
+}
+
+
+ do while statement
+
+ 
+
+class DoWhileDemo {
+   public static void main(String[] args)
+
+
+{
+       int count = 1;
+       do {
+           System.out.println("Count is: " + count);
+           count++;
+       } while (count < 11);
+   }
+}
