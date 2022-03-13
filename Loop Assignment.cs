b4770f12186cd9d0d3ee887059b95d5af474e70a
channpreet1991 <65122644+channpreet1991@users.noreply.github.com>
2022-03-13 13:46:31 -0300

    2nd commit

--- Loop Assignment.cs
+++ Loop Assignment.cs
@@ -1,32 +1,40 @@
+//while
 
-while statement
+bool nameValidation = false;
+while (nameValidation == false) {
+Console.Write("Enter your name: ");  // Asks for your name
+userName = Console.ReadLine();
 
- 
-
-class WhileDemo {
-   public static void main(String[] args){
-       int count = 1;
-       while (count < 11) {
-           System.out.println("Count is: " + count);
-           count++;
-       }
-   }
-}
-
-
- do while statement
-
- 
-
-class DoWhileDemo {
-   public static void main(String[] args)
-
-
+if (Regex.IsMatch(userName, @"^[a-zA-Z- ]+$"))  // Validates the input containts characters and/or spaces
 {
-       int count = 1;
-       do {
-           System.out.println("Count is: " + count);
-           count++;
-       } while (count < 11);
-   }
+   nameValidation = true;              
 }
+else  // Error message if the input is not valid
+{
+   Console.Clear();  // Clear screen
+   Console.WriteLine("Please enter a valid name.");
+   nameValidation = false;
+ }
+}
+
+//do
+
+using System;
+
+namespace Loops {
+  class Program {
+     static void Main(string[] args) {
+        /* local variable definition */
+        int a = 10;
+        
+        /* do loop execution */
+        do {
+           Console.WriteLine("value of a: {0}", a);
+           a = a + 1;
+        } 
+        while (a < 20);
+        Console.ReadLine();
+     }
+  }
+} 
+
